10a29c73e1c6149e671d3b25bd2f2d5625e80689
Kimsitos <kimsitos15@gmail.com>
2025-10-05 14:03:19 +0200

    ft_strlcat done

---
+++ ft_strlcat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stcozaci <stcozaci@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/05 13:15:04 by stcozaci          #+#    #+#             */
+/*   Updated: 2025/10/05 14:02:49 by stcozaci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
+{
+    size_t  x;
+    size_t  y;
+    
+    x = ft_strlen(dst);
+    y = x;
+    while (x < size - 1 && src[x - y])
+    {
+        dst[x] = src [x - y];
+        x++;
+    }
+    if (y < size)
+        dst[x] = '\0';
+    return (y + ft_strlen(src));
+}
