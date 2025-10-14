7f1917201f735345c5b3a4085fe1cf229f1fbb0a
Stefan Adrian Cozaciuc <stcozaci@c2r17s2.42madrid.com>
2025-10-14 20:15:52 +0200

    libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: stcozaci <stcozaci@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 16:30:33 by stcozaci          #+#    #+#             */
-/*   Updated: 2025/10/13 15:26:36 by stcozaci         ###   ########.fr       */
+/*   Updated: 2025/10/14 12:30:15 by stcozaci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int		ft_tolower(int c);
 
 //string.h
 char	*ft_strchr(const char *str, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
@@ -33,6 +34,8 @@ size_t	ft_strlen(const char *str);
 char	*ft_strrchr(const char *str, int c);
 char	*ft_strnstr(const char *big, const char *small, size_t size);
 void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
@@ -55,11 +58,4 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
-//bonus
-typedef s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
 #endif
